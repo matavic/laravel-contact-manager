07009dca0bda62f50d50507471ead0d70a17300f
Vicente Mata <matavic25@hotmail.com>
2018-08-04 16:29:55 -0400

    Added crud api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Contact;
 use Illuminate\Http\Request;
 
 /*
@@ -11,7 +12,44 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
+Route::group(['middleware' => 'api'], function () {
+    // Fetch Contacts
+    Route::get('contacts', function () {
+        return Contact::latest()->orderByDesc('created_at')->get();
+    });
+
+    // Get single Contact
+    Route::get('contact/{id}', function ($id) {
+        return Contact::findOrFail($id);
+    });
+
+    // Add Contact
+    Route::post('contact/store', function (Request $request) {
+        return Contact::create([
+            'name' => $request->input('name'),
+            'phone' => $request->input('phone'),
+            'email' => $request->input('email'),
+        ]);
+    });
+
+    // Update Contact
+    Route::patch('contact/{id}', function (Request $request, $id) {
+
+        Contact::findOrFail($id)->update([
+            'name' => $request->input('name'),
+            'phone' => $request->input('phone'),
+            'email' => $request->input('email'),
+        ]);
+    });
+
+    // Delete Contact
+    Route::delete('contact/{id}', function ($id) {
+
+        return Contact::destroy($id);
+    });
+
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
