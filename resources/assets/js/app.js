6d6edbcb1162354f26336cd3eb98170c7e6b1d1f
Vicente Mata <matavic25@hotmail.com>
2018-08-04 14:09:13 -0400

    Added Contact Form Vue Component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,9 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('contacts', require('./components/Contacts.vue'));
 
 const app = new Vue({
-    el: '#app'
+  el: '#app'
 });
---
+++ resources/assets/js/components/Contacts.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <h1>Contacts</h1>
+        <form action="#" @submit.prevent="edit ? updateContact(contact.id) : createContact()">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input v-model="contact.name" type="text" name="name" id="name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input v-model="contact.email" type="email" name="phone" id="phone" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="phone">Phone</label>
+                <input v-model="contact.phone" type="text" name="phone" id="phone" class="form-control">
+            </div>
+            <div class="form-group">
+                <button v-show="!edit" type="submit" class="btn btn-primary">New Contact</button>
+                <button v-show="edit" type="submit" class="btn btn-primary">Update Contact</button>
+            </div>
+        </form>
+    </div>    
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      edit: false,
+      list: [],
+      contact: {
+        id: '',
+        name: '',
+        email: '',
+        phone: ''
+      }
+    };
+  },
+  mounted() {
+    console.log('Contacts Component loaded.');
+  },
+  methods: {
+    createContact: function() {
+      console.log('Creating Contact...');
+      return;
+    },
+    updateContact: function(id) {
+      console.log('Editing Contact ' + id + ' ...');
+      return;
+    }
+  }
+};
+</script>
